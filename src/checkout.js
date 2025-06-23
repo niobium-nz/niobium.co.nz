df4e08f450db162354b8ff2871038a4f911f6191
Wen Han <hcp5he11@gmail.com>
2025-06-24 00:47:53 +1200

    Test.

--- src/checkout.js
+++ src/checkout.js
@@ -52,7 +52,7 @@ async function handleSubmit(e) {
     //    method: "POST",
     //});
     //const { instruction } = await response.json();
-    const instruction = "pi_3Rd8KgRt21XMpal806utY9E1_secret_7e57kzIHfn9wI1IdL1BtUJ8oq";
+    const instruction = "pi_3Rd9mfRt21XMpal8101gX2Nt_secret_9pBDuzq4BUJcEdHZ26TNTej2z";
     const { error } = await stripe.confirmPayment({
         elements,
         clientSecret: instruction,
